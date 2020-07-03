2642bdd521d5f925c0c55044d167a333640b3b0b
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-07-03 20:00:49 +0300

    Master

---
+++ Object Pooling Example/Assets/Scripts/BulletTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTest : MonoBehaviour
+{
+    public GameObject BulletPrefab; //oluşturduğum prefab'in atamasını Inspector'dan yapacağım.
+    private Pool _pool;
+    [SerializeField]
+    public GameObject _turret;
+
+
+    private void Start()
+    {
+        _pool = new Pool(BulletPrefab);
+        _pool.FillPool(5); //Havuzu start kısmında dolduruyorum. 
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulletObject = _pool.UnloadPool();
+            bulletObject.gameObject.SetActive(true);
+            StartCoroutine(BulletDestory(4f, bulletObject));
+        }
+    }
+
+    IEnumerator BulletDestory(float min, GameObject _bullet)
+    {
+        yield return new WaitForSeconds(min);
+        _pool.AddPool(_bullet);
+    }
+
+}
---
+++ Object Pooling Example/Assets/Scripts/Pool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool
+{
+    private GameObject _poolPrefab;
+    private Stack<GameObject> pool = new Stack<GameObject>(); //havuzu Stack ile oluşturduk.
+
+    public Pool(GameObject poolPrefab)
+    {
+        _poolPrefab = poolPrefab; 
+    }
+
+    public void FillPool(int limit)
+    {
+        for(int i = 0; i< limit; i++)
+        {
+            GameObject poolObject = Object.Instantiate(_poolPrefab);
+            AddPool(poolObject);
+        }
+    }
+
+
+    public void AddPool(GameObject poolObject)
+    {
+        poolObject.gameObject.SetActive(false);
+        pool.Push(poolObject); //Stack'e objeyi atıyoruz.
+    }
+
+    public GameObject UnloadPool()
+    {
+        if(pool.Count > 0)
+        {
+            GameObject _object = pool.Pop();
+            return _object;
+        }
+
+        return Object.Instantiate(_poolPrefab);
+    }
+}
